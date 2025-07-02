f55a5e9d40013b7854f6fe67bf2cfccd9527695c
Mohd-Arzaid <arzaid0101030@gmail.com>
2025-07-02 11:44:55 +0530

    fix

--- src/common/Navbar.jsx
+++ src/common/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import companyLogo from "../assets/company-logo/company-logo.png";
+import companyLogo from "../assets/company-logo/company-logo.webp";
 import {
   NavigationMenu,
   NavigationMenuContent,
